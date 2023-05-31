c03d83d3a2e9dcf38ad2f0d623c3f44043fe390a
Rony Romero Duque @ronygrafo <ronygrafo@icloud.com>
2023-05-31 18:22:25 -0500

    Examen Parcial Terminado!

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/prop-types */
+
+import { useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 
 function App() {
 
+  const [nombre, setNombre] = useState("")
+  const [perfil, setPerfil] = useState("")
+
+  const [show, setShow] = useState(false)
+  const [error, setError] = useState(null)
+
+  /* USE VALIDACION DEL SEGUNDO INPUT >7, POR EL STORYTELLING */
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(nombre.length > 3 && perfil.length > 7){
+      setShow(true)
+      setError(false)
+    } else {
+      setShow(false)
+      setError(true)
+    }
+  }
+
+
+
   return (
     <>
     <div className='app'>
-      <h2>Carga de estudiantes</h2>
-        <form></form>
-      <Card/>
+
+      <h2>Tu Carnet de Developer</h2>
+
+        <form onSubmit={handleSubmit}>
+          <label >Tu nombre completo:</label>
+          <input type="text" onChange={ (e)=> setNombre(e.target.value)}/>
+
+          <label >Tu Perfil como Dev?</label>
+          <input type="text" onChange={ (e)=> setPerfil(e.target.value)}/>
+          <p>Colocá "Frontend" o "Backend"</p> <br />
+
+          <button type="submit" >
+            Crear Carnet
+          </button>
+        </form>
+
+      {show && <Card nombre={nombre} perfil={perfil}/>}
+      {error && <p className='warning'>“Por favor chequea que la información sea correcta”</p>}
+
     </div>
     </>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/prop-types */
+
 import './Card.css'
 
-const Card = () => {
+const Card = ({nombre, perfil}) => {
   return (
     <div className='card'>
-        Hola, soy una tarjeta
+        <h1>{nombre}</h1>
+        <h2>{perfil}</h2>
     </div>
   )
 }
